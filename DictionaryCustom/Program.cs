8546388fb06d58461858e0b009e35fa614164365
cemilcan sağdıç <cemilcansagdic@hotmail.com>
2021-01-23 11:48:29 +0300

    Add project files.

---
+++ DictionaryCustom/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DictionaryCustom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "123");
+            myDictionary.Add(2, "234");
+            myDictionary.Add(3, "567");
+            Console.WriteLine(myDictionary.Get(1));
+            Console.ReadLine();
+            
+
+        }
+    }
+}
